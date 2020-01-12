7534dc2ee72d6e08a17e6180cf9cb988383cf4c6
rlaqud9k <rlaqud9k@gmail.com>
2020-01-12 06:54:53 +0000

    end

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 var canvas = document.querySelector("#jsCanvas");
+var ctx = canvas.getContext("2d");
+canvas.strokeStyle  =  ' # 666 ' ; 
+canvas.lineWidth  =  9 ;
+let start = false;
 
 function xyFinder(event){
   var x = event.offsetX;
   var y = event.offsetY;
-  console.log(x, y);
+  if(!start){     
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  }else{
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+ 
+  
+  console.log(x, y, start);
 }
-
+function mDown(x, y){
+  start = true;
+}
+function mUp(e){
+  start = false;
+}
+function mOut(e){
+  start = false;
+}
 canvas.addEventListener("mousemove", xyFinder);
+canvas.addEventListener("mousedown", mDown);
+canvas.addEventListener("mouseup", mUp);
+canvas.addEventListener("mouseout", mOut);
